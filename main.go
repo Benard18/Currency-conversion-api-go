8ad6018e1f76c620644995bb2b8a89013b0af8db
Ben Karanja <benkaranja43@gmail.com>
2018-11-04 15:44:23 +0300

    This is my backend file for the listing the current versions in the world based on currencies

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"log"
+	)
+type Response struct{
+	Date string  `json:"date"`
+	Base string `json:"base"`
+	TimeStamps int `json:"timestamp"`
+	Rates interface{} `json:"rates"` 
+}
+
+func main() {
+	response, err := http.Get("https://bankersalgo.com/apirates2/5bded30ac7ad0/236930aa9daf69ab232db85a1d42c609/EUR")
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		data, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var responseObject Response
+	json.Unmarshal(data, &responseObject)
+
+	 // fmt.Println(responseObject.Rates)
+	for key, value := range responseObject.Rates.(map[string]interface{}) {
+	 fmt.Println(key,value)
+
+	}
+
+	}
+
+
+
+}
